d56195101a409d8939e04a1fc06c8806ce2adc62
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2020-03-06 13:08:14 +0500

    App Component Structure Setup

--- src/container/App.js
+++ src/container/App.js
@@ -5,11 +5,46 @@ import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component {
+  state = {
+    id: uuid(),
+    item: '',
+    items: [
+      { id: 1, title: 'Wake Up' },
+      { id: 2, title: 'Make Breakfast' }
+    ],
+    editItem: false
+  };
+  handleInputChange = e => {
+    console.log(e);
+  };
+  handleFormSubmit = e => {
+    e.preveantDefault();
+  };
+  clearList = () => {};
+  handleDelete = id => {};
+  handleEdit = id => {};
   render() {
+    const { id, item, items, editItem } = this.state;
     return (
-      <div>
-        <TodoInput />
-        <TodoList />
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={item}
+              handleInputChange={this.handleInputChange}
+              handleFormSubmit={this.handleFormSubmit}
+              editItem={editItem}
+            />
+            <TodoList
+              items={items}
+              key={id}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
+        </div>
       </div>
     );
   }
