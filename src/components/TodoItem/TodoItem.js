b9ac2c642e2bef1f4a2c9e5208bb3fd3378095f0
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2020-03-06 18:39:21 +0500

    Added component logic

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export default function TodoItem() {
+export default function TodoItem(props) {
   return (
-    <div>
-      <h1>Hello From Todo Item</h1>
-    </div>
+    <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+      <h6>{props.title}</h6>
+      <div className="todo-icon">
+        <span className="mx-2 text-success" onClick={props.handleEdit}>
+          <i className="fas fa-pen"></i>
+        </span>
+        <span className="mx-2 text-danger" onClick={props.handleDelete}>
+          <i className="fas fa-trash"></i>
+        </span>
+      </div>
+    </li>
   );
 }
