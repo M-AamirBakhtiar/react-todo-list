2b70244ace7d370757043731f022f31b2042c02f
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2020-03-06 18:39:48 +0500

    Added component Logic

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,11 +3,25 @@ import TodoItem from '../TodoItem/TodoItem';
 
 export default class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <>
-        <h2>Todo List</h2>
-        <TodoItem />
-      </>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
+        {items.map(item => (
+          <TodoItem
+            key={item.id}
+            title={item.title}
+            handleDelete={() => handleDelete(item.id)}
+            handleEdit={() => handleEdit(item.id)}
+          />
+        ))}
+        <button
+          className="btn btn-danger btn-block text-uppercase mt-5"
+          onClick={clearList}
+        >
+          clear List
+        </button>
+      </ul>
     );
   }
 }
